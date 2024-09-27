5c81613e2c56f801cdd4e3bc4212e4a108ac4e64
Jorge Leonardo Hernandez Rozo <seergee18@gmail.com>
2024-09-26 23:38:47 -0500

    update background-color of user profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from 'react-redux'
+import { PageContainer } from '../components/PageContainer'
 
 export const Profile = () => {
     const {
@@ -10,11 +11,12 @@ export const Profile = () => {
     if (is_loading) return <p>Verificando sesión...</p>
 
     return is_authenticated && user ? (
-        <div>
+        <PageContainer
+            background={'radial-gradient(circle, rgb(94 43 125) 30%, rgb(61 36 92) 100%)'}>
             <h2>Bienvenido, {user.fullname}</h2>
             <p>Nombre de usuario: {user.username}</p>
             <p>Rol: {user.role}</p>
-        </div>
+        </PageContainer>
     ) : (
         <p>No se pudo cargar la información del usuario</p>
     )
